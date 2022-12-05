f76f050408f016785c155fe2c215cde87330ee39
Flora Preuvot <flora@qomon.com>
2022-12-05 09:28:15 +0100

    update after solution

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
-    <div v-for="level in 7" class="flex flex-row justify-center">
-      <!-- This div is styled for the tree sections, but it's not complete -->
-      <div
-        v-for="branch in descendNum(level)"
-        class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center"
-      />
+    <ChristmasTree v-if="size > 1" :size="size - 1" />
+    <div class="flex flex-row justify-center">
+      <div v-for="i in size" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
     </div>
   </div>
 </template>
@@ -19,13 +16,4 @@ withDefaults(
     size: 1,
   }
 )
-
-function descendNum(startNum) {
-  let nextNum = startNum - 1
-
-  if (nextNum >= 0) {
-    descendNum(nextNum)
-    return startNum
-  }
-}
 </script>
