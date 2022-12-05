983fe67302b2c1482275d8c5bd21690ce793b55e
Flora Preuvot <flora@qomon.com>
2022-12-05 09:24:24 +0100

    first commit

---
+++ src/ChristmasTree.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="level in 7" class="flex flex-row justify-center">
+      <!-- This div is styled for the tree sections, but it's not complete -->
+      <div
+        v-for="branch in descendNum(level)"
+        class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size: number
+  }>(),
+  {
+    size: 1,
+  }
+)
+
+function descendNum(startNum) {
+  let nextNum = startNum - 1
+
+  if (nextNum >= 0) {
+    descendNum(nextNum)
+    return startNum
+  }
+}
+</script>
